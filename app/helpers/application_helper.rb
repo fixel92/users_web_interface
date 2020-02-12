83c194e4e7385de8dfbc727e9a84dc57b32878b7
fixel92 <goodzoon92@gmail.com>
2020-02-12 21:56:11 +0300

    updated design

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,38 @@
 module ApplicationHelper
+  def flash_class(level)
+    case level
+    when 'notice' then 'alert alert-info'
+    when 'success' then 'alert alert-success'
+    when 'error' then 'alert alert-danger'
+    when 'alert' then 'alert alert-warning'
+    end
+  end
+
   def user_avatar(user)
     if user.avatar?
       user.avatar.url
     else
       asset_path('avatar.jpg')
     end
+  end
+
+  def user_avatar_thumb(user)
+    if user.avatar.file.present?
+      user.avatar.thumb.url
+    else
+      asset_path('avatar.jpg')
+    end
+  end
+
+  def user_avatar_min(user)
+    if user.avatar.file.present?
+      user.avatar.min.url
+    else
+      asset_path('avatar.jpg')
+    end
+  end
+
+  def fa_icon(icon_class)
+    content_tag 'span', '', class: "fa fa-#{icon_class}"
   end
 end
--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -31,6 +31,10 @@ class AvatarUploader < CarrierWave::Uploader::Base
   # Create different versions of your uploaded files:
   version :thumb do
     process resize_to_fit: [100, 100]
+    end
+
+  version :min do
+    process resize_to_fit: [30, 30]
   end
 
   # Add a white list of extensions which are allowed to be uploaded.
