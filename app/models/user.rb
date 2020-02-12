4811d94c45b9840a04cc4e368dc88589197aea29
fixel92 <goodzoon92@gmail.com>
2020-02-12 09:34:09 +0300

    added devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,6 @@
 class User < ApplicationRecord
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
   validates :name, presence: true, length: { minimum: 5, maximum: 35 }
 end
